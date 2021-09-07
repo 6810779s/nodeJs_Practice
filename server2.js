ca22235f8a950009eba451ac9c9f3d529768059f
EUNHEE JANG <6810779s@naver.com>
2021-09-07 23:08:45 +0900

    practice

--- server2.js
+++ server2.js
@@ -1,18 +1,32 @@
 const http = require("http");
 const fs = require("fs");
+const info = require("./info.js");
 const app = http.createServer((request, response) => {
   const _url = request.url;
   const fullUrl = new URL("http://localhost:3000" + _url);
   const pathName = fullUrl.pathname;
+  const queryData = fullUrl.searchParams;
 
   if (pathName === "/") {
     let list = "";
+    if (queryData.get("id") === null) {
+      title = `<h1>코드짜는 문과녀</h1>`;
+      description = `<p>코드짜는 문과녀의 블로그입니다.</p>`;
+    } else {
+      /* localhost:3000/?id=NodeJs 일때,
+        queryData.get("id") = NodeJs
+      */
+      title = queryData.get("id");
+      description = fs.readFileSync(`./data/${title}`, "utf8"); 
+    }
+
     fs.readdir("./data", (err, files) => {
       files.forEach((file) => {
-
+        list += `<li><a href="/?id=${file}">${file}</a></li>`;
       });
+      response.writeHead(200, { "Content-Type": "text/html;charset = utf-8" });
+      response.end(info(title, description, list));
     });
-    fs.readFile
   }
 });
 
